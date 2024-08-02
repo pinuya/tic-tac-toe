8dde236575261066ebac8d57f4582841d0613105
Tifany <tifsnunes@gmail.com>
2024-08-02 13:29:58 -0300

    validate function checkWinnerFor

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ function itemClick(event) {
   }
 }
 
+checkGame();
+
 function reset() {
   warning = "";
 
@@ -63,3 +65,44 @@ function togglePlayer() {
   player = player === "x" ? "o" : "x";
   renderInfo();
 }
+
+function checkGame() {
+  if (checkWinnerFor("x")) {
+    warning = 'O "X" venceu!';
+    playing = false;
+  } else if (checkWinnerFor("o")) {
+    warning = 'O "O" venceu!';
+    playing = false;
+  } else if (isFull()) {
+    warning = "Deu empate!";
+    playing = false;
+  }
+}
+
+function checkWinnerFor(player) {
+  let pos = [
+    "a1,a2,a3",
+    "b1,b2,b3",
+    "c1,c2,c3",
+    "a1,b1,c1",
+    "a2,b2,c2",
+    "a3,b3,c3",
+    "a1,b2,c3",
+    "a3,b2,c1",
+  ];
+
+  for (let w in pos) {
+    let pArray = pos[w].split(","); // a1, a2, a3
+    let hasWon = pArray.every((option) => {
+      square[option] === player;
+
+      if (hasWon) {
+        return true;
+      }
+    });
+  }
+
+  return false;
+}
+
+function isFull() {}
